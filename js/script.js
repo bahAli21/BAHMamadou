cf5a5518009487dc86f54563fe4c9267535fd1d6
Mamadou BAH <bah311802@gmail.com>
2024-05-09 19:20:53 +0200

    Update portfolio

--- js/script.js
+++ js/script.js
@@ -71,7 +71,7 @@ ScrollReveal({
 
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
 ScrollReveal().reveal('.home-img, .services-container, .project-box, .contact form', { origin: 'bottom' });
-ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content h1, .title .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 
